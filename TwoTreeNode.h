3a217e6ade9dd87ee881674ee2dae1a60acf9739
Terianne Bolding <tleblanc@github.com>
2016-01-12 16:07:59 -0700

    added using for unique_ptr

--- TwoTreeNode.h
+++ TwoTreeNode.h
@@ -9,11 +9,13 @@
 #ifndef TWOTREENODE_H
 #define TWOTREENODE_H
 #include <memory>
+using std::unique_ptr;
+
 template <class T>
 class TwoTreeNode {
     public:
-    std::unique_ptr<TwoTreeNode> leftChild;
-    std::unique_ptr<TwoTreeNode> rightChild;
+    unique_ptr<TwoTreeNode> leftChild;
+    unique_ptr<TwoTreeNode> rightChild;
     const T &key;
     TwoTreeNode(const T &_key, TwoTreeNode  *_leftChild, TwoTreeNode *_rightChild) : leftChild(_leftChild),rightChild(_rightChild),key(_key){}
     TwoTreeNode(const T &_key): key(_key){}
